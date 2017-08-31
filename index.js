ce999a258b91c70743fc9ad72324b6dff66a48d2
Joel Rojas Soliz <joel.rojas.soliz@gmail.com>
2017-08-31 16:42:36 -0400

    Add Cors middleware to be used in Restify server

--- index.js
+++ index.js
@@ -1,27 +1,35 @@
 var restify = require('restify');
 var apiFns = require('./api');
+const corsMiddleware = require('restify-cors-middleware');
 
-function corsHeaders(req, res, next) {
-  req.setRequestHeader('Access-Control-Request-Headers', "Origin, Accept, X-Requested-With, Content-Type");
-  req.setRequestHeader('Access-Control-Request-Method', "GET, POST, OPTIONS");
-  res.setHeader('Access-Control-Allow-Origin', "*");
-  res.setHeader('Access-Control-Allow-Headers', "Origin, Accept, X-Requested-With, Content-Type");
-  res.setHeader('Access-Control-Allow-Methods', "GET, POST, OPTIONS");
-  next();
-}
-function serveOptions(req, res, next) {
-    res.send(200);
-}
+const cors = corsMiddleware({
+    preflightMaxAge: 5, //Optional
+    origins: ['*'],
+    allowHeaders: ['*'],
+    exposeHeaders: ['*']
+});
+
+// function moreOpts(req, res, next) {
+//   res.header('Access-Control-Allow-Origin', "*");
+//   res.header('Access-Control-Allow-Headers', "Origin, Accept, X-Requested-With, Content-Type");
+//   res.header('Access-Control-Allow-Methods', "GET, POST, OPTIONS");
+//   res.send(200);
+//   return next();
+// }
+// function serveOptions(req, res, next) {
+//     res.send(200);
+// }
 
 var port = 9090;
 var server = restify.createServer();
-
-server.use(corsHeaders);
-
+server.pre(cors.preflight);
+server.use(cors.actual);
+// server.use(restify.CORS());
+// server.opts(/.*/, moreOpts);
 server.get('/api/output_data', apiFns.getOutputData);
 server.post('/api/pdf', apiFns.createPDFFile);
 server.post('/api/csv', apiFns.createCSVFile);
-server.opts(/./, serveOptions);
+
 
 server.listen(port, function() {
   console.log('%s listening at %s', server.name, server.url);
